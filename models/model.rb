aa8c2d8c4f5b851464f8438316bb91571301bbb7
cassiebensko03 <cassidymbensko@gmail.com>
2018-07-20 18:28:34 +0000

    automatically backed up by learn

--- models/model.rb
+++ models/model.rb
@@ -1,35 +1,4 @@
-require_relative 'config/environment'
-
-class App < Sinatra::Base
-
-# configure do
-#   set :views, "/views"
-#   set :public_dir, "public"
-# end
-
-  get '/' do
-    erb :index
-  end
-  
-  get '/' do
-    rb :model
-end
-
-  # Add your post route and action below
-  post '/' do
-    #the first part we can make up the 2ns part is already assigned in index
-    the_genre = params[:user_genre]
-    the_issue = params[:user_issue]
-    @song 
-    # = get_anthem(the_genre,the_issue)
-    erb :results
-  end
-end
-    
-    
-    
-
-
+#this IS where ruby goes
 def get_anthem(genre,issue)
   
   if genre == "rock"
@@ -72,9 +41,11 @@ def get_anthem(genre,issue)
         elsif issue == "self love"
         song = "https://media.giphy.com/media/l3q2GeMfc5MnHGseI/giphy.gif"
         elsif issue == "girlboss"
-        song = "https://media.giphy.com/media/WtaBktbOpnOKY/giphy.gif"
+        song = "https://media.giphy.com/media/l3q2GeMfc5MnHGseI/giphy.gif"
        end
   end
   
-@song = song
+return song
 end
+
+puts get_anthem("rnb","empowerment")
